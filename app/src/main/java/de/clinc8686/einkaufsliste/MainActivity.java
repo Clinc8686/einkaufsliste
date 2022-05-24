5f9e8d04e65657e70c8edebd782386c3a700b161
Mario <mario1.lanpert@gmail.com>
2022-05-24 22:35:59 +0200

    fixed Bug Toast empty

--- app/src/main/java/de/clinc8686/einkaufsliste/MainActivity.java
+++ app/src/main/java/de/clinc8686/einkaufsliste/MainActivity.java
@@ -42,6 +42,8 @@ public class MainActivity extends AppCompatActivity {
                 string.append(actv.getText()).append(", ");
             }
         }
-        Toast.makeText(MainActivity.this, string.deleteCharAt(string.length()-2), Toast.LENGTH_LONG).show();
+
+        if(string.length() > 0)
+            Toast.makeText(MainActivity.this, string.deleteCharAt(string.length()-2), Toast.LENGTH_LONG).show();
     }
 }
